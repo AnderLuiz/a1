700afb1b4daf1f8653a5152dda82fa79d0e16119
Anderson <andersonlven@gmail.com>
2019-05-06 16:31:12 +0000

    Corrigir setTimeout

--- calc/index.js
+++ calc/index.js
@@ -1,11 +1,27 @@
-export const sum = (a, b) => a + b;
+export const sum = async (a, b) => {
+  const timeout = parseInt((Math.random() * 5000) + 1000, 10);
+  await new Promise(done => setTimeout(() => done(), timeout));
+  return a + b;
+};
 
-export function sub(a, b) {
+export async function sub(a, b) {
+  await setTimeout(() => { }, 1000);
   return a - b;
 }
 
-export const mult = function mult(a, b) {
+export const mult = async function mult(a, b) {
+  const timeout = parseInt((Math.random() * 5000) + 1000, 10);
+  await new Promise(done => setTimeout(() => done(), timeout));
   return a * b;
+};
+
+export const div = async (a, b) => {
+  if (b === 0) {
+    throw new Error('O denominador não pode ser 0');
+  }
+  const timeout = parseInt((Math.random() * 5000) + 1000, 10);
+  await new Promise(done => setTimeout(() => done(), timeout));
+  return a / b;
 };
 
 // export const div = (a, b) => new Promise((resolve, reject) => {
@@ -14,13 +30,5 @@ export const mult = function mult(a, b) {
 //   }
 //   setTimeout(() => { resolve(a / b); }, 3000);
 // });
-
-export const div = async (a, b) => {
-  if (b === 0) {
-    throw new Error('O denominador não pode ser 0');
-  }
-  await setTimeout(() => { }, 1000);
-  return a / b;
-};
 
 export default sum;
